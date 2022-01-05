677bef6059cd62ecec9d74a044cc53c7d0367ecd
carlcris <coder387@gmail.com>
2022-01-05 11:57:50 +0800

    convert return fields to camelCase

--- model/patient.go
+++ model/patient.go
@@ -7,22 +7,22 @@ import (
 )
 
 type Patient struct {
-	PatientId string
-	FirstName string
-	LastName  string
-	MI        string
-	BirthDate string
-	Email     string
-	Phone     string
-	SSN       string
+	PatientId string  `json:"patientId"`
+	FirstName string  `json:"firstName"`
+	LastName  string  `json:"lastName"`
+	MI        string  `json:"mi"`
+	BirthDate string  `json:"birthDate"`
+	Email     string  `json:"email"`
+	Phone     string  `json:"phone"`
+	SSN       string  `json:"ssn"`
 	Address   Address `gorm:"embedded"`
 }
 type Address struct {
-	AddressId string
-	Address1  string
-	City      string
-	State     string
-	Zip       string
+	AddressId string `json:"addressId"`
+	Address1  string `json:"address1"`
+	City      string `json:"city"`
+	State     string `json:"state"`
+	Zip       string `json:"zip"`
 }
 
 func (p *Patient) SavePatient() (*Patient, error) {
@@ -69,12 +69,16 @@ func GetPatientByID(c *gin.Context) {
 			Select("patient.patient_id, patient.first_name, patient.last_name, patient.mi, patient.birth_date, patient.ssn, patient.phone, patient.email, address.address_id, address.address1, address.city, address.state, address.zip").
 			Joins("JOIN address ON patient.patient_id = address.patient_id").
 			Where("patient.patient_id =?", uid).
-			Find(&p)
+			First(&p)
+
 		if result.Error != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 			return
 		}
-
+		if result.RecordNotFound() {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Record Not Found"})
+			return
+		}
 		c.JSON(http.StatusOK, p)
 	}
 }
